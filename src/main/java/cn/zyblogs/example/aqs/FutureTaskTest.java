a1f23d1a7298999d4a88dd60bc36c3cd492cdd9f
zhangyibo1012 <35024391+zhangyibo1012@users.noreply.github.com>
2018-10-13 15:41:45 +0800

    fork/join

---
+++ src/main/java/cn/zyblogs/example/aqs/FutureTaskTest.java
@@ -0,0 +1,30 @@
+package cn.zyblogs.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @Title: FutureTaskTest.java
+ * @Package cn.zyblogs.example.aqs
+ * @Description: TODO OK
+ * @Author ZhangYB
+ * @Version V1.0
+ */
+@Slf4j
+public class FutureTaskTest {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            log.info("do something in callable" );
+            Thread.sleep(5_000);
+            return "Done.";
+        });
+
+        new Thread(futureTask).start();
+        log.info("do something in main" );
+        Thread.sleep(1_000);
+        String result = futureTask.get();
+        log.info("result：{}", result );
+    }
+}
---
+++ src/main/java/cn/zyblogs/example/aqs/FutureTest.java
@@ -0,0 +1,37 @@
+package cn.zyblogs.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * @Title: FutureTest.java
+ * @Package cn.zyblogs.example.aqs
+ * @Description: TODO
+ * @Author ZhangYB
+ * @Version V1.0
+ */
+@Slf4j
+public class FutureTest {
+
+    static class MyCallable implements Callable<String>{
+        @Override
+        public String call() throws Exception {
+            log.info("do something in callable" );
+            Thread.sleep(5_000);
+            return "Done.";
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Future<String> future = executor.submit(new MyCallable());
+        log.info("do something in main" );
+        Thread.sleep(1_000);
+        // 没有执行完成会等待
+        String result = future.get();
+        log.info("result：{}", result );
+
+
+    }
+}
